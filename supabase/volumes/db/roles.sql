ea1c4a08bd88e78dd94ece479dee0a1307961eb6
jonathan-arceus <119608072+jonathan-arceus@users.noreply.github.com>
2026-01-19 18:06:47 +0530

    fix: db passwords and kong entrypoint

--- supabase/volumes/db/roles.sql
+++ supabase/volumes/db/roles.sql
@@ -3,11 +3,11 @@ create role anon noinherit nologin;
 create role authenticated noinherit nologin;
 create role service_role noinherit nologin bypassrls;
 create role supabase_admin noinherit nologin superuser;
-create role supabase_auth_admin noinherit nologin nobypassrls;
-create role supabase_storage_admin noinherit nologin nobypassrls;
+create role supabase_auth_admin noinherit login password 'your-super-secret-password' nobypassrls;
+create role supabase_storage_admin noinherit login password 'your-super-secret-password' nobypassrls;
 create role dashboard_user noinherit nologin;
 
-create role authenticator noinherit login noreplication;
+create role authenticator noinherit login password 'your-super-secret-password' noreplication;
 grant anon to authenticator;
 grant authenticated to authenticator;
 grant service_role to authenticator;
